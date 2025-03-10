8ded0271732fec1ef7b55660d313223ee1c50096
GuilhermeFuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-03-10 08:05:28 -0300

    Add files via upload

---
+++ (3)0a20par.c
@@ -0,0 +1,14 @@
+/*programas do caralho*/
+#include <stdio.h>
+
+int main(){
+    int n = 0;
+    while(n<=20){
+        if(n%2==0){
+            printf("%d \n", n);
+            n++;
+        }else{
+            n++;
+        }
+    }
+}
