f62c4f313b698f2fa7ae0265b009ee5a545f4753
GuilhermeFuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-03-10 08:40:35 -0300

    Add files via upload

---
+++ (10)nome.c
@@ -0,0 +1,9 @@
+/*programas do caralho*/
+#include <stdio.h>
+
+int main(){
+    char nome[50];
+    printf("qual é o seu nome? \n");
+    scanf("%s", &nome);
+    printf("seja bem-vindo %s! você é maneirao!", nome);
+}
