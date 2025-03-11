e82d96b57707131b1084340022ae9dc7ab131b05
GuilhermeFuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-03-11 11:04:38 -0300

    Add files via upload

---
+++ (18)primo.c
@@ -0,0 +1,25 @@
+/*lista de exercícios*/
+#include <stdio.h>
+
+int main(){
+    int n;
+    int i = 2;
+    printf("digite um número: ");
+    scanf("%d", &n);
+    while(i<n){
+        if(n%i==0){
+            printf("%d não é primo", n);
+            break;
+        }else if(i==(n-1)){
+            printf("%d é primo", n);
+            break;
+        }else if(n==2){
+            printf("%d é primo", n);
+            break;
+        }else{
+            i++;
+        }
+    }
+
+    return 0;
+}
