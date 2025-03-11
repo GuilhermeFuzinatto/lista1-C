e783477a78096ae4fc3dfbd575d072c5b62df026
GuilhermeFuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-03-11 19:34:57 -0300

    Update (18)primo.c

--- (18)primo.c
+++ (18)primo.c
@@ -6,6 +6,10 @@ int main(){
     int i = 2;
     printf("digite um número: ");
     scanf("%d", &n);
+    if(n<=3){
+        printf("%d é primo", n);
+        return 0;
+    }
     while(i<n){
         if(n%i==0){
             printf("%d não é primo", n);
